c949fe0064e943cdc7750cbe4e676d93d505bf13
RomainPerdriat <romainperdriat@gmail.com>
2022-02-17 19:44:57 +0100

    ajout d'un état changeant au click sur bouton DM

--- src/components/BoutonDm/BoutonDm.jsx
+++ src/components/BoutonDm/BoutonDm.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './boutonDm.scss';
 import { Button } from '@mui/material';
 import ButtonHeader from '../ButtonHeader/ButtonHeader';
 
+
+
 const Bouton_DM = ({className, ...rest}) => {
+
+    const [isClick, setIsClick] = useState(false);
+    console.log('Etat du bouton', isClick);
+
+    const handleClick = () => {
+        console.log(`On a cliqué sur le bouton`)
+        setIsClick(!isClick)
+    }
    return (
-        <Button variant="contained" color="success">
-              DarkMode
-       </Button>
-       
-            
-       
+        <Button 
+            variant="contained" 
+            color="success"
+            onClick= {handleClick}
+            >
+            {(!isClick ? 'BeachMode' : 'ForestMode') }
+              
+       </Button>     
    );
 };
 
--- todo.md
+++ todo.md
@@ -1,3 +1,4 @@
-
+Résoudre le problème de position du footer: doit être partout le meme.
+résoudre le pb d'import des images depuis les datas.
 Mettre les contacts avec des liens vers mail github  et linkedIn
 Travailler sur le playamode 
